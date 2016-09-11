626ce848639bdc104988c400ae6790cd2d6d0805
Eric Brown <quadtree@gmail.com>
2016-09-11 23:58:05 +0000

    EOL fixes

---
+++ SpellCastLogEntry.js
@@ -0,0 +1,10 @@
+"use strict";
+
+class SpellCastLogEntry
+{
+    constructor(spellName, turnNumber, target){
+        this.spellName = spellName;
+        this.turnNumber = turnNumber;
+        this.target = target;
+    }
+}
