dd2178013172c53ff71d51d7246ce64e4a1348ba
Eric Brown <quadtree@gmail.com>
2016-09-16 08:27:26 +0000

    Delayed action kind of sucks

--- controllers/assassin/delayed_action.js
+++ controllers/assassin/delayed_action.js
@@ -17,24 +17,6 @@ function turn(turnNumber){
         let attackedTheWizard = [];
         let wizardsHP = null;
 
-        for (let p of participants){
-            if (p.name == wizardsName){
-                wizardsHP = p.hp;
-                continue;
-            }
-
-            if (p.hp <= 0) continue;
-
-            for (let scle of p.spellCastLog){
-                let theSpell = SPELLS[scle.spellName];
-                if (!theSpell) throw "Wait, spell " + scle.spellName + " doesn't exist!";
-                if (theSpell.damage > 0 && (scle.target == wizardsName || theSpell.targetMode == "ALL")){
-                    attackedTheWizard.push(p.name);
-                    break;
-                }
-            }
-        }
-
         // if we found no targets, then just attack anyone not the wizard randomly
         if (attackedTheWizard.length == 0){
             for (let p of PARTICIPANTS){
