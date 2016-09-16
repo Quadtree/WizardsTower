96abc1c76ee60321e13b09430b75cd179efbe55f
Eric Brown <quadtree@gmail.com>
2016-09-16 08:30:49 +0000

    Assistor added

---
+++ controllers/assassin/assistor.js
@@ -0,0 +1,95 @@
+let wizardsName = null;
+
+function turn(turnNumber){
+    let participants = PARTICIPANTS;
+    let me = ME;
+
+    if (wizardsName === null){
+        for (let p of participants){
+            if (p.charClass == "WIZARD"){
+                wizardsName = p.name;
+                break;
+            }
+        }
+    }
+
+    let attackedTheWizard = [];
+    let wizardsHP = null;
+
+    for (let p of participants){
+        if (p.name == wizardsName){
+            wizardsHP = p.hp;
+            continue;
+        }
+
+        if (p.hp <= 0) continue;
+
+        for (let scle of p.spellCastLog){
+            let theSpell = SPELLS[scle.spellName];
+            if (!theSpell) throw "Wait, spell " + scle.spellName + " doesn't exist!";
+            if (theSpell.damage > 0 && (scle.target == wizardsName || theSpell.targetMode == "ALL")){
+                attackedTheWizard.push(p.name);
+                break;
+            }
+        }
+    }
+
+    if (attackedTheWizard.length > 0){
+        // someone has attacked the wizard, so let's help!
+        let potentialTargets = [];
+        let wizardIsAlive = false;
+
+        for (let p of participants){
+            if (p.name == me.name) continue;
+            if (p.hp <= 0) continue;
+
+            potentialTargets.push(p.name);
+
+            if (p.name == wizardsName && p.hp > 0) wizardIsAlive = true;
+        }
+        if (wizardIsAlive){
+            return {
+                spell: "Flame Arrow",
+                target: wizardsName,
+            };
+        } else {
+            if (potentialTargets.length > 0){
+                return {
+                    spell: "Flame Arrow",
+                    target: potentialTargets[Math.floor(Math.random() * potentialTargets.length)]
+                };
+            }
+            return {};
+        }
+    } else {
+        let potentialTargets = [];
+
+        let isWizardAlive = null;
+
+        for (let p of PARTICIPANTS){
+            if (p.name == ME.name) continue;
+            if (p.hp <= 0) continue;
+            if (p.charClass == "WIZARD") continue;
+
+            potentialTargets.push(p.name);
+        }
+
+        if (potentialTargets.length == 0){
+            for (let p of PARTICIPANTS){
+                if (p.name == ME.name) continue;
+                if (p.hp <= 0) continue;
+
+                potentialTargets.push(p.name);
+            }
+        }
+
+        if (potentialTargets.length > 0){
+            return {
+                spell: "Flame Arrow",
+                target: potentialTargets[Math.floor(Math.random() * potentialTargets.length)]
+            };
+        }
+
+        return {};
+    }
+}
